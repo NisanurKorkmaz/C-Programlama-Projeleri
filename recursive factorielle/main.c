28a46512c4270daa2ccaee82c72570c62cba4fe5
Nisanur Korkmaz <80275552+NisanurKorkmaz@users.noreply.github.com>
2021-11-23 15:14:43 +0300

    Add files via upload

---
+++ recursive factorielle/main.c
@@ -0,0 +1,27 @@
+//
+//  main.c
+//  recursive factorielle
+//
+//  Created by Nisanur Korkmaz on 11/05/2020.
+//  Copyright © 2020 Nisanur Korkmaz. All rights reserved.
+//
+
+#include <stdio.h>
+
+int fact(int sayi)
+{
+    if(sayi<=0)
+        return 1 ;
+    else
+        return sayi*fact(sayi-1);
+}
+int main()
+{
+    int sayi ;
+    printf("sayi giriniz : ");
+    scanf("%d",&sayi);
+    printf("girdiginiz sayinin factorieli : %d\n ",fact(sayi));
+    
+    
+    return 0;
+}
