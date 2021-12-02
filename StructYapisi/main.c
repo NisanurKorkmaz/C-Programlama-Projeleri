39916a88a94bfc6a82af842d7ad904beb379220b
Nisanur Korkmaz <80275552+NisanurKorkmaz@users.noreply.github.com>
2021-12-02 11:16:47 +0300

    Add files via upload

---
+++ StructYapisi/main.c
@@ -0,0 +1,40 @@
+//
+//  main.c
+//  20.2
+//
+//  Created by Nisanur Korkmaz on 22/12/2020.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+struct node {
+    char isim[10];
+    int yas;
+    int kilo;
+    float boy;
+    struct node *next;
+    
+};
+struct node *ilk=NULL, *son=NULL;
+
+int main() {
+//    struct node oyuncular[3];
+    char isim[10];
+    int y;
+    int k;
+    float b;
+    FILE *dosya;
+    dosya = fopen("oyuncu.txt","r" );
+    if(dosya == NULL)
+                perror("File Not Found ");
+           else {
+    while( fscanf(dosya," %s %d %d %f ",isim ,&y, &k, &b)!=EOF)
+    {
+        printf("%s  %d  %d  %.2f \n",isim,y,k,b);
+    
+    }
+                }
+    
+    fclose(dosya);
+    return 0;
+}
